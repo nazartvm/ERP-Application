85cd02c9b0abdac6edac8c19cca8e56d6aae086c
Nazartvm <nazar@aktrea.com>
2019-11-30 17:48:46 +0530

    'first-commit'

---
+++ ERPApplication.DomainModels/Company/Company.cs
@@ -0,0 +1,22 @@
+﻿using ERPApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace ERPApplication.DomainModels.Company
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public string CompanyName { get; set; }
+        public int BaseCurrency { get; set; }
+        public int CreatedBy { get; set; }
+        [ForeignKey("CreatedBy")]
+        public virtual ApplicationUser Users
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ ERPApplication/Data/Migrations/20191130114555_Added-CompanyTable.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ERPApplication.Data.Migrations
+{
+    public partial class AddedCompanyTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Companies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    BaseCurrency = table.Column<int>(nullable: false),
+                    CompanyName = table.Column<string>(nullable: true),
+                    CreatedBy = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Companies", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Companies_AspNetUsers_CreatedBy",
+                        column: x => x.CreatedBy,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Companies_CreatedBy",
+                table: "Companies",
+                column: "CreatedBy");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Companies");
+        }
+    }
+}
